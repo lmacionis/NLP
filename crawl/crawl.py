11cefa82a538c848ab0c7b025cf8997797bc251d
lmacionis <lmacionis@github.com>
2025-09-29 09:05:26 +0300

    Recreated class Crawl, added methods get_data, load_data, get_link_title

--- crawl/crawl.py
+++ crawl/crawl.py
@@ -7,32 +7,48 @@ from requests import HTTPError
 URL = "https://www.geeksforgeeks.org/python/python-programming-language-tutorial/"
 
 
-class Links:
+class Crawl:
     def __init__(self, source):
         self.source = source
 
-    def get_link(self):
+    # Extracting html data from web page and saving to txt file.
+    def get_data(self):
         response = requests.get(self.source)
 
         if response.status_code == 200:
             try:
                 text = response.text
                 assert isinstance(text, str)
-
-                tree = HTML(text)
-                links = tree.xpath('//div[@class="text"]//a[1]/@href') # returns dtype list
-
-                return links
+                with open("./data/html_data.txt", "w") as file:
+                    file.write(text)
+                    file.close()
 
             except Exception as error:
                 raise TypeError(f"Wrong data type: {error}")
         else:
             raise HTTPError(f"Page is unreachable: {response.status_code}")
 
+    # Open data from html_data.txt file
+    def load_data(self):
+        path = "./data/html_data.txt"
+        with open(path, "r") as file:
+            html = file.read()
+            file.close()
 
-get_link = Links(URL).get_link()
-print(get_link)
+        return html
 
-# file = open("crawl/ex.txt", "w")
-# file.write(resp.text)
-# file.close()
+    # Get links and titles for tutorials from web page.
+    def get_link_title(self):
+        tree = HTML(self.load_data())   # str
+        links = tree.xpath('//div[@class="text"]//a/@href') # returns dtype list
+        titles = tree.xpath('//div[@class="text"]//a/span')
+        titles_text = [title.text for title in titles]
+        
+        return links, titles_text
+
+
+# get_data = Crawl(URL).get_data()
+
+get_link_title = Crawl(URL).get_link_title()
+print(get_link_title)
+
