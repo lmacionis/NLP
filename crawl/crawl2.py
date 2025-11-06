52411ed1f856922a1b4de2a401a964cd3d425284
lmacionis <lmacionis@github.com>
2025-11-06 14:16:21 +0200

    created crawl2.py to extract data from diferent type of web pages.

--- crawl/crawl2.py
+++ crawl/crawl2.py
@@ -6,7 +6,9 @@ import pandas as pd
 import numpy as np
 
 
-URL = "https://www.geeksforgeeks.org/python/python-programming-language-tutorial/"
+# URL = "https://www.geeksforgeeks.org/python/python-programming-language-tutorial/"
+URL = "https://www.w3schools.com/python/default.asp"
+url_path = "https://www.w3schools.com/python/"
 
 
 class Crawl:
@@ -41,16 +43,24 @@ class Crawl:
     # Get links and titles for tutorials from web page.
     def get_link_title(self):
         tree = HTML(self.load_data())   # str
-        links = tree.xpath('//div[@class="text"]//a/@href') # returns dtype list
-        titles = tree.xpath('//div[@class="text"]//a/span')
+        links = tree.xpath('//div[@id="leftmenuinnerinner"]//a/@href') # returns dtype list
+        titles = tree.xpath('//div[@id="leftmenuinnerinner"]//a')
         titles_text = [title.text for title in titles]
         
         return links, titles_text
 
     def text(self):
         tree = HTML(self.load_data())   # str
-        info = tree.xpath('//div[@class="text"]//span/text() | //div[@class="text"]//strong/text() | //div[@class="text"]//a/@href')
-
+        info = tree.xpath('//div[@id="main"]/h1/text() | \
+                            //div[@id="main"]//span/text() | \
+                            //div[@id="main"]//h2/text() | \
+                            //div[@id="main"]//p/text() | \
+                            //div[@id="main"]//div[@class="w3-code notranslate w3-black"]/text() | \
+                            //div[@id="main"]//div[@class="w3-code notranslate w3-black"]/em/text() | \
+                            //div[@id="main"]//h3/text() | \
+                            //div[@class="w3-code notranslate pythonHigh"]/text() | \
+                            //div[@class="w3-code notranslate pythonHigh w3-border-red"]/text() | \
+                            //div[@id="main"]//code/text()')
         return info
 
 
@@ -60,21 +70,9 @@ get_data = Crawl(URL).get_data()
 
 # Take titles and URL'S and form csv file.
 link, title = Crawl(URL).get_link_title()
-URL = link[6]
+URL = url_path + link[10]
 get_data = Crawl(URL).get_data()
 print(URL)
 info = Crawl(URL).text()
 info = ' '.join(info)
 print(info)
-
-# def save_as_csv(link, title):
-#     if len(title) != len(link):
-#         # Adding NaN to fix diference between link's and titles, that we could transform it into DataFrame. 
-#         title.extend(["NaN"] * (len(link) - len(title)))
-#         dict_for_csv = {"Title": title, "Link": link}
-    
-#     df = pd.DataFrame(dict_for_csv)    
-#     df.index += 1
-#     df.to_csv("./data/data_as.csv")
-
-# save_as_csv(link, title)
